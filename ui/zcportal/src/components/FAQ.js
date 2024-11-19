bd64f54d8e3f311c9147296495f685188875d7f2
AsmaaJAH <asmaagamal.nagy@gmail.com>
2024-11-19 11:27:00 +0200

    solving an issue and everything is working as expected

--- ui/zcportal/src/components/FAQ.js
+++ ui/zcportal/src/components/FAQ.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { variables } from './Variables.js';
 import { AddButton } from './AddButton';
+import { OptionsCell } from './OptionsCell';
 
 
 export class FAQ extends Component {
@@ -71,7 +72,7 @@ export class FAQ extends Component {
             PhotoFileName: "anonymous.PNG",
         });
     }
-    editClick(question) {
+    editClick=(question)=> {
         this.setState({
             modalTitle: "Edit FAQ",
             Id: question.Id,
@@ -129,7 +130,7 @@ export class FAQ extends Component {
             })
     }
 
-    deleteClick(id) {
+    deleteClick=(id)=> {
         if (window.confirm('Are you sure you wanna delete this FAQ ?')) {
             fetch(variables.API_URL + 'FAQ/' + id, {
                 method: 'DELETE',
@@ -187,11 +188,6 @@ export class FAQ extends Component {
         return (
             <div>
                 <AddButton addClick={() => this.addClick()} />
-
-                
-
-
-
                 <table className='table table-striped'>
                     <thead>
                         <tr>
@@ -220,28 +216,11 @@ export class FAQ extends Component {
                                 <td>{question.QuestionTitle}</td>
                                 <td>{question.Answer}</td>
                                 <td>{question.PostingDate}</td>
-                                <td>
-                                    <button type='button'
-                                        className='btn btn-light mr-1'
-                                        data-bs-toggle="modal" data-bs-target="#exampleModal"
-                                        onClick={() => this.editClick(question)}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                                        </svg>
-                                    </button>
-                                    {/*Spacer div*/}
-                                    <div style={{ width: '10px', display: 'inline-block' }}></div>
-
-                                    <button type='button'
-                                        className='btn btn-light mr-1'
-                                        onClick={() => this.deleteClick(question.Id)}
-                                    >
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
-                                            <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
-                                        </svg>
-                                    </button>
-                                </td>
+                                <OptionsCell
+                                    question={question}
+                                    editClick={this.editClick}
+                                    deleteClick={this.deleteClick}
+                                />
                             </tr>
                         )}
                     </tbody>
