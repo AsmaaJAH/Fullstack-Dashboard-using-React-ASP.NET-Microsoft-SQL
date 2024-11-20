bf625c53bd8ed1aef730ceecebc35ef149d70cee
AsmaaJAH <asmaagamal.nagy@gmail.com>
2024-11-20 20:47:30 +0200

    databse

--- zcportal.sql
+++ zcportal.sql
@@ -38,15 +38,34 @@ create table dbo.FAQ(
 Id int identity(1,1),
 QuestionTitle nvarchar(500), 
 Answer nvarchar(500),
-PostingDate	 datetime,
+/* PostingDate	 datetime,
 PhotoFileName nvarchar(500),
-UrlLink nvarchar(1000)
+UrlLink nvarchar(1000)*/
+
 )
 
-insert into dbo.FAQ values ('how to do something?', 'IT', GETDATE(), 'anonymous.png')
-insert into dbo.FAQ values ('how to register a course?', 'IT', GETDATE(), 'anonymous.png')
+insert into dbo.FAQ values ('how to do something?', 'seek help from IT'/*, GETDATE(), 'anonymous.png'*/)
+insert into dbo.FAQ values ('how to register a course?', 'seek help from registrar'/*, GETDATE(), 'anonymous.png'*/)
 
 
 
 select * from dbo.FAQ
 
+/*---------------------- Announcement ------------------------*/
+create table dbo.Announcement(
+Id int identity(1,1),
+Title nvarchar(500), 
+Content nvarchar(500),
+PostingDate	 datetime,
+PhotoFileName nvarchar(500),
+)
+
+insert into dbo.Announcement values ('Event1', 'Event content should be here..!', GETDATE(), 'anonymous.png')
+insert into dbo.Announcement values ('Event2', 'Event content should be here', GETDATE(), 'anonymous.png')
+insert into dbo.Announcement values ('Event3', 'Event content should be here..!', GETDATE(), 'anonymous.png')
+insert into dbo.Announcement values ('Event4', 'Event content should be here', GETDATE(), 'anonymous.png')
+
+
+
+
+select * from dbo.Announcement
